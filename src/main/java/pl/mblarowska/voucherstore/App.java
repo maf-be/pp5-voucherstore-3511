b7a1f1ab12b315e4542f337dc73d68b4ffe52bc4
maf-be <monikablarowska@gmail.com>
2021-02-01 01:54:42 +0100

    add spring dep and epxose product catalog as rest ep

--- src/main/java/pl/mblarowska/voucherstore/App.java
+++ src/main/java/pl/mblarowska/voucherstore/App.java
@@ -1,7 +1,11 @@
 package pl.mblarowska.voucherstore;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("It works");
+        SpringApplication.run(App.class, args);
     }
 }
