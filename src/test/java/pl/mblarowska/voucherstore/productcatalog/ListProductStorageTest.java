b62ad464b08d70e506a17395edf10a1dd052e44f
maf-be <monikablarowska@gmail.com>
2021-02-01 02:16:26 +0100

    add jdbc, test dep and missing files

---
+++ src/test/java/pl/mblarowska/voucherstore/productcatalog/ListProductStorageTest.java
@@ -0,0 +1,25 @@
+package pl.mblarowska.voucherstore.productcatalog;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+
+public class ListProductStorageTest {
+    @Test
+    public void itAllowStoreProduct() {
+        Product p1 = thereIsProduct();
+        ProductStorage storage = new ListProductStorage();
+
+        storage.save(p1);
+
+    }
+
+    private Product thereIsProduct() {
+        Product product = new Product(UUID.randomUUID());
+        product.setPrice(BigDecimal.valueOf(10));
+
+        return product;
+    }
+}
