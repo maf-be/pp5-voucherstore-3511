d86e5015b2673e2094ee25754c1adbde407d5d3c
maf-be <monikablarowska@gmail.com>
2021-02-02 01:39:09 +0100

    handle create reservation

--- src/main/java/pl/mblarowska/voucherstore/sales/SalesFacade.java
+++ src/main/java/pl/mblarowska/voucherstore/sales/SalesFacade.java
@@ -9,6 +9,7 @@ import pl.mblarowska.voucherstore.sales.offer.OfferMaker;
 import pl.mblarowska.voucherstore.sales.ordering.ClientData;
 import pl.mblarowska.voucherstore.sales.ordering.OfferChangedException;
 import pl.mblarowska.voucherstore.sales.ordering.Reservation;
+import pl.mblarowska.voucherstore.sales.ordering.ReservationRepository;
 import pl.mblarowska.voucherstore.sales.payment.PaymentDetails;
 import pl.mblarowska.voucherstore.sales.payment.PaymentGateway;
 import pl.mblarowska.voucherstore.sales.payment.PaymentUpdateStatusRequest;
@@ -22,14 +23,16 @@ public class SalesFacade {
     Inventory inventory;
     OfferMaker offerMaker;
     PaymentGateway paymentGateway;
+    private final ReservationRepository reservationRepository;
 
-    public SalesFacade(ProductCatalogFacade productCatalogFacade, InMemoryBasketStorage basketStorage, CurrentCustomerContext currentCustomerContext, Inventory inventory, OfferMaker offerMaker, PaymentGateway paymentGateway) {
+    public SalesFacade(ProductCatalogFacade productCatalogFacade, InMemoryBasketStorage basketStorage, CurrentCustomerContext currentCustomerContext, Inventory inventory, OfferMaker offerMaker, PaymentGateway paymentGateway, ReservationRepository reservationRepository) {
         this.productCatalogFacade = productCatalogFacade;
         this.basketStorage = basketStorage;
         this.currentCustomerContext = currentCustomerContext;
         this.inventory = inventory;
         this.offerMaker = offerMaker;
         this.paymentGateway = paymentGateway;
+        this.reservationRepository = reservationRepository;
     }
 
     public void addProduct(String productId1) {
@@ -65,6 +68,8 @@ public class SalesFacade {
         Reservation reservation = Reservation.of(currentOffer, clientData);
 
         PaymentDetails reservationPaymentDetails = paymentGateway.register(reservation);
+
+        reservationRepository.save(reservation);
 
         return  reservationPaymentDetails;
     }
--- src/test/java/pl/mblarowska/voucherstore/sales/SalesTestCase.java
+++ src/test/java/pl/mblarowska/voucherstore/sales/SalesTestCase.java
@@ -5,6 +5,8 @@ import pl.mblarowska.voucherstore.productcatalog.ProductCatalogConfiguration;
 import pl.mblarowska.voucherstore.productcatalog.ProductCatalogFacade;
 import pl.mblarowska.voucherstore.sales.basket.InMemoryBasketStorage;
 import pl.mblarowska.voucherstore.sales.offer.OfferMaker;
+import pl.mblarowska.voucherstore.sales.ordering.InMemoryReservationRepository;
+import pl.mblarowska.voucherstore.sales.ordering.ReservationRepository;
 import pl.mblarowska.voucherstore.sales.payment.DummyPaymentGateway;
 import pl.mblarowska.voucherstore.sales.payment.PaymentGateway;
 import pl.mblarowska.voucherstore.sales.product.ProductDetails;
@@ -21,6 +23,7 @@ public class SalesTestCase {
     String customerId;
     OfferMaker offerMaker;
     PaymentGateway paymentGateway;
+    ReservationRepository reservationRepository;
 
     protected CurrentCustomerContext thereIsCurrentCustomerContext() {
         return () -> customerId;
@@ -62,6 +65,10 @@ public class SalesTestCase {
         return id;
     }
 
+    protected ReservationRepository thereIsInMemoryReservationsRepository() {
+        return new InMemoryReservationRepository();
+    }
+
     protected SalesFacade thereIsSalesModule() {
         return new SalesFacade(
                 productCatalog,
@@ -69,7 +76,8 @@ public class SalesTestCase {
                 currentCustomerContext,
                 inventory,
                 offerMaker,
-                paymentGateway
+                paymentGateway,
+                reservationRepository
         );
     }
 }
